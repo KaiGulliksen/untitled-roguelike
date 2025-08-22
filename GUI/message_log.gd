e4de32803672987da7d2bc437578f7fa6607cf25
Kai Gulliksen <mail@kaigulliksen.com>
2025-08-23 00:20:37 +0200

    Optimized message scrolling code

--- GUI/message_log.gd
+++ GUI/message_log.gd
@@ -14,44 +14,18 @@ static func send_message(text: String, color: Color) -> void:
 	SignalBus.message_sent.emit(text, color)
 
 func add_message(text: String, color: Color) -> void:
-	if (
-		last_message != null and 
-		last_message.plain_text == text
-	):
+	if last_message != null and last_message.plain_text == text:
 		last_message.count += 1
 	else:
 		var message := Message.new(text, color)
 		last_message = message
 		message_list.add_child(message)
 	
-	# Multiple deferred calls to ensure scrolling happens after all UI updates
-	call_deferred("_prepare_scroll")
+	# Single deferred call
+	call_deferred("_scroll_to_bottom")
 
-func _prepare_scroll() -> void:
-	# Force the container to update its size
-	message_list.queue_sort()
-	call_deferred("_do_scroll")
-
-func _do_scroll() -> void:
-	# Wait one more frame to be absolutely sure
+func _scroll_to_bottom() -> void:
 	await get_tree().process_frame
-	
-	# Force scroll to bottom with multiple approaches
-	var v_scrollbar = get_v_scroll_bar()
-	if v_scrollbar:
-		# Set to max value
-		scroll_vertical = v_scrollbar.max_value
-		# Force the scrollbar to update
-		v_scrollbar.value = v_scrollbar.max_value
-		
-	# Also try ensure_control_visible as backup
-	if last_message and is_instance_valid(last_message):
-		ensure_control_visible(last_message)
-		
-	# One more deferred update just to be sure
-	call_deferred("_final_scroll")
-
-func _final_scroll() -> void:
 	var v_scrollbar = get_v_scroll_bar()
 	if v_scrollbar:
 		scroll_vertical = v_scrollbar.max_value
