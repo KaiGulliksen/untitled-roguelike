0e88d8e651d166d69a42276fd766fd6e81796314
Kai Gulliksen <mail@kaigulliksen.com>
2025-08-27 22:21:16 +0200

    Fixed portal entry code

--- Entities/Actors/Actions/enter_portal_action.gd
+++ Entities/Actors/Actions/enter_portal_action.gd
@@ -6,7 +6,7 @@ func perform() -> bool:
 	var current_tile: Tile = map_data.get_tile(entity.grid_position)
 
 	if current_tile and current_tile.is_portal():
-		var game_node = entity.get_tree().get_root().get_node("InterfaceRoot/VBoxContainer/HBoxContainer/SubViewportContainer/SubViewport/Game")
+		var game_node = entity.get_tree().get_root().find_child("Game", true, false)
 		if game_node:
 			game_node.call_deferred("_enter_dungeon")
 		MessageLog.send_message("You enter the portal!", GameColors.WELCOME_TEXT)
